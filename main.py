acb80d992eb85d5353d252a45516bcc467c39a01
alenapopova <alenap525@gmail.com>
2025-03-19 10:14:00 +0300

    chore: fastapi init

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
 import psycopg2
 from psycopg2 import sql
+
+from random import randint
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+
 host = '79.174.88.238'
-port=15221
-db_name='school_db'
-user='school'
-password='School1234*'
+port = 15221
+db_name = 'school_db'
+user = 'school'
+password = 'School1234*'
 
 conn = psycopg2.connect(
-  host=host,
-  port=port,
-  dbname=db_name,
-  user=user,
-  password=password
+    host=host,
+    port=port,
+    dbname=db_name,
+    user=user,
+    password=password
 
 )
 
-cbs=conn.cursor()
+cur = conn.cursor()
 print(" соединение с БД установлено")
-with open("schema.sql","r") as file:
-  sql_script=file.read()
-cbs.execute(sql_script)
+with open("schema.sql", "r") as file:
+    sql_script = file.read()
+cur.execute(sql_script)
 conn.commit()
 print(" таблица сохранена")
 
+app = FastAPI(openapi_prefix="/api/v1/")
+
+
+class Car(BaseModel):
+    model: str
+    year: int = Field(ge=1700)
+    color: str
+    type: str
+
+
+
+@app.get("/cars")
+def cars(car: Car):
+
+    id = randint(0, 999999)
+
+    query = """
+        INSERT INTO popova_chepelev.cars (id, model, car_year, color, type)
+        VALUES (%s, %s, %s, %s, %s);
+    """
+    values = (id, car.model, car.year, car.color, car.type)
+
+    cur.execute(query, values)
+    conn.commit()
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "succesful"
+        }
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
--- schema.sql
+++ schema.sql
@@ -1,11 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS popova_chepelev;
-CREATE TABLE  IF NOT EXISTS popova_chepelev.CARS (
-  Id INTEGER PRIMARY KEY,
+CREATE TABLE  IF NOT EXISTS popova_chepelev.cars (
+  id INTEGER PRIMARY KEY,
   model TEXT NOT NULL,
   car_year INTEGER NOT NULL,
   color TEXT NOT NULL,
-  type TEXT NOT NULL,
-  accident TEXT NOT NULL
+  type TEXT NOT NULL
 );
-
-
